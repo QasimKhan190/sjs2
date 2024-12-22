a67ab1f9223c64f446fc57a971a28642cd676bb2
QasimKhan190 <qasimkamran17@gmail.com>
2024-12-23 00:38:20 +0500

    Add files via upload

---
+++ smitjs2/s.js
@@ -0,0 +1,36 @@
+var username=1
+
+MYNAME="qasim khan"
+
+message="hello world"
+
+alert(message)
+
+a = "jhone doe"
+b="15 years old"
+c="certified mobile application devlopment"
+alert(a)
+alert(b)
+alert(c)
+
+
+
+p="PIZZA\nPIZZ\nPIZ\nPI\nP\n"
+alert(p)
+
+email="abc@gmail.com"
+alert(email)
+
+
+
+
+book="i am trying to learn from book A smarter way to javascript"
+alert(book)
+
+
+
+document.write("Yah! I can write html content through javascript");
+
+
+d="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(d)
